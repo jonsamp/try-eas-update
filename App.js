e1202a6d8d0cf0cf54c0acde4a011e735655386b
jonsamp <sampjon@gmail.com>
2021-08-25 16:00:30 -0400

    charlie

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image source={ToolsImage} style={styles.image} />
-      <Text>Update 2</Text>
+      <Text>Update Charlie!</Text>
       <StatusBar style='auto' />
     </View>
   );
