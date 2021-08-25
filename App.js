f30ccb0ad07a322e1bd7c6b706011a1de8f21ea8
jonsamp <sampjon@gmail.com>
2021-08-25 12:59:28 -0400

    adds and image and text update

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ToolsImage from './assets/tools.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={ToolsImage} style={styles.image} />
+      <Text>Update 1</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 350,
+    height: 300,
+    resizeMode: 'cover',
+    marginBottom: 32,
   },
 });
